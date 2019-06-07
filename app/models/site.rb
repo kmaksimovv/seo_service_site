27bf45ea4e397865009a29b109cc4d47389ad77e
Maksimov Konstantin <kmaksimovv@gmail.com>
2019-06-07 13:44:15 +0400

    fixes

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,5 @@
 class Site < ApplicationRecord
   belongs_to :user
-  has_many :sitemap_pages
 
   validates :domain, presence: true, uniqueness: { case_sensitive: false }
   validates :user, presence: true
