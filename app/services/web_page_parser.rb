ac3003283838a985819835cd58acdd691f111eea
Maksimov Konstantin <kmaksimovv@gmail.com>
2019-07-05 15:47:04 +0400

    module WebPageParser

--- app/services/web_page_parser.rb
+++ app/services/web_page_parser.rb
@@ -9,8 +9,31 @@ module WebPageParser
       @doc = Nokogiri::HTML(open(url))
     end
 
+  # high tags
     def title
       @doc.title
     end
+
+    def charset
+      @doc.at('meta[charset]')&.values&.join
+    end
+
+    def compatible
+      @doc.at('meta[http-equiv]').values.join(',')
+    end
+
+    def viewport
+      @doc.at('meta[name = "viewport"]').values.join(',')
+    end
+
+    def description
+      @doc.at('meta[name = "description"]').values.second
+    end
+
+    def language_tag
+      @doc.children[1].attribute('lang').value
+    end
+
+    # low tags
   end
 end
