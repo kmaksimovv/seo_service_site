6c83d7890c36f699241271bb91ce04820696003e
Maksimov Konstantin <kmakimovv@gmail.com>
2019-09-23 22:59:34 +0400

    truck offconfirmation devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_many :sites, dependent: :destroy
 end
